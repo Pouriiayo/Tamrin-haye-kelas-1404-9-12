3112a605dad09fc6c3901e98d14e502e89a1106f
pouria yousefi daneshjoo <164668228+Pouriiayo@users.noreply.github.com>
2025-12-09 23:17:48 +0330

    Add files via upload

---
+++ Tamrin 5/Tamrin 5/Check6174.cs
@@ -0,0 +1,56 @@
+﻿namespace check;
+
+public static class Check6174
+{
+    public static bool CheckIsRepeated(int n)
+    {
+        string number = n.ToString();
+
+        bool IsRepeated = number.Distinct().Count() == 1;
+
+        return IsRepeated;
+    }
+
+    public static int CheckNumIs6174(int n)
+    {
+        int D = n;
+        while (D != 6174 && !CheckIsRepeated(n))
+        {
+
+            int big = SortNumBigtoSmall(D);
+            int small = SortNumSmalltoBig(D);
+
+            D = big - small;
+            
+            if (CheckIsRepeated(D))
+            {
+                break;
+            }
+
+            Console.Write(D + " ");
+        }
+        return D;
+    }
+
+    private static int SortNumBigtoSmall(int n)
+    {
+        char[] Digits = n.ToString().ToCharArray();
+        Array.Sort(Digits);
+        Array.Reverse(Digits);
+
+        string Sortnum = new string(Digits);
+
+        return Convert.ToInt32(Sortnum);
+
+    }
+    private static int SortNumSmalltoBig(int n)
+    {
+        char[] Digits = n.ToString().ToCharArray();
+        Array.Sort(Digits);
+
+        string Sortnum = new string(Digits);
+
+        return Convert.ToInt32(Sortnum);
+
+    }
+}
---
+++ Tamrin 5/Tamrin 5/Program.cs
@@ -0,0 +1,8 @@
+﻿using check;
+int x = 1000;
+int y = 9999;
+
+for (int i = 1000; i <= 9999; i++)
+{
+        Console.WriteLine($"({i}) => {Check6174.CheckNumIs6174(i)}");
+}
